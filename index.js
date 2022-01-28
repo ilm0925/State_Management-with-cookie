29d8836fbe63438d49683a387b0c7f2ec4244b44
leejw05 <zeta050525@gmail.com>
2022-01-28 21:34:44 +0900

    complate

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   const cookie = req.cookies;
 
   console.log(cookie.NAME);
-  if (cookie.NAME == null && cookie.ID && cookie.PWD == null) {
+  if (cookie.NAME == null && cookie.ID == null && cookie.PWD == null) {
     res.send(views.main(null, 0));
   } else {
     res.send(views.main(cookie.NAME, 1));
@@ -30,7 +30,7 @@ app.post("/login", async (req, res) => {
   const auth = Auth(body.id, body.pwd);
   console.log(auth);
   if (auth == false) {
-    res.send("<script>alert('다시 로그인 해주세요')</script>");
+    res.send("<script>alert('다시 로그인 해주세요');history.back();</script>");
   } else {
     res.set({
       "Set-Cookie": [`ID=${body.id}`, `PWD=${body.pwd}`, `NAME=${auth}`],
@@ -38,7 +38,12 @@ app.post("/login", async (req, res) => {
     res.redirect("/");
   }
 });
-
+app.post("/logout", (req, res) => {
+  res.clearCookie("ID");
+  res.clearCookie("PWD");
+  res.clearCookie("NAME");
+  res.redirect("/");
+});
 app.listen(3000, () => {
   console.log(`open Sever ${3000}`);
 });
--- view/html.js
+++ view/html.js
@@ -12,7 +12,15 @@ export default class view {
   }
   main(name, state) {
     if (state == 1) {
-      return `<h1 style="text-align: center">안녕하세요 ${name}님</h1>`;
+      return `
+      <form action="/logout" method="POST">
+      <h1 style="text-align: center">안녕하세요 ${name}님
+      <br/>
+      <br/>
+      <button type="submit">로그아웃</button>
+      </h1>
+      </form>
+      `;
     }
     return `<h1 style="text-align: center"><a href="/login">로그인</a>을하세요</h1>`;
   }
